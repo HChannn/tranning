5f08d3bad92c31fa9444dd7c93abf2a0ab7bc0a6
huy.luong <luonganhhuy5@gmail.com>
2024-09-29 23:41:43 +0700

    review Topic_02

--- src/test/java/Training/Topic_02_WebBrowser.java
+++ src/test/java/Training/Topic_02_WebBrowser.java
@@ -10,10 +10,21 @@ import org.testng.annotations.*;
 public class Topic_02_WebBrowser extends BaseTest {
     WebDriver driver;
 
-    @Test
-    public void TC_01_Verify_url() {
+    // Before Class để chạy hàm trong khối annotation before này đầu tiên, tránh việc khởi tạo nhiều lần
+    @BeforeClass
+    public void beforeClass() {
         driver = getBrowserDriver("chrome");
         driver.get("http://live.techpanda.org/");
+    }
+
+    // After Class mỗi khi class chaỵ xong sẽ chạy vào khối annotation after này
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    @Test
+    public void TC_01_Verify_url() {
         driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
 
         String actualUrlLoginPage = driver.getCurrentUrl();
@@ -27,59 +38,48 @@ public class Topic_02_WebBrowser extends BaseTest {
         System.out.println("Url: " + actualUrlRegisterPage);
         String expectedUrlRegisterPage = "http://live.techpanda.org/index.php/customer/account/create/";
         Assert.assertEquals(actualUrlRegisterPage, expectedUrlRegisterPage);
-
-        driver.quit();
     }
 
-@Test
-public void TC_02_Verify_title() {
-    driver = getBrowserDriver("chrome");
-    driver.get("http://live.techpanda.org/");
-    driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+    @Test
+    public void TC_02_Verify_title() {
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
 
-    String actualTitleLoginPage = driver.getTitle();
-    System.out.println("Title: " + actualTitleLoginPage);
-    String expectedTitleLoginPage = "Customer Login";
-    Assert.assertEquals(actualTitleLoginPage, expectedTitleLoginPage);
+        String actualTitleLoginPage = driver.getTitle();
+        System.out.println("Title: " + actualTitleLoginPage);
+        String expectedTitleLoginPage = "Customer Login";
+        Assert.assertEquals(actualTitleLoginPage, expectedTitleLoginPage);
 
-    driver.findElement(By.xpath("//a[@title = 'Create an Account']")).click();
+        driver.findElement(By.xpath("//a[@title = 'Create an Account']")).click();
 
-    String actualTitleRigisterPage = driver.getTitle();
-    System.out.println("Title: " + actualTitleRigisterPage);
-    String expectedTitleRigisterPage = "Create New Customer Account";
-    Assert.assertEquals(actualTitleRigisterPage, expectedTitleRigisterPage);
-
-    driver.quit();
+        String actualTitleRigisterPage = driver.getTitle();
+        System.out.println("Title: " + actualTitleRigisterPage);
+        String expectedTitleRigisterPage = "Create New Customer Account";
+        Assert.assertEquals(actualTitleRigisterPage, expectedTitleRigisterPage);
     }
 
-@Test
-public void TC_03_Navigate_Function() throws InterruptedException {
-    driver = getBrowserDriver("chrome");
-    driver.get("http://live.techpanda.org/");
-    driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
-    driver.findElement(By.xpath("//a[@title = 'Create an Account']")).click();
+    @Test
+    public void TC_03_Navigate_Function() {
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        driver.findElement(By.xpath("//a[@title = 'Create an Account']")).click();
 
-    String actualUrlRegisterPage = driver.getCurrentUrl();
-    System.out.println("Url: " + actualUrlRegisterPage);
-    String expectedUrlRegisterPage = "http://live.techpanda.org/index.php/customer/account/create/";
-    Assert.assertEquals(actualUrlRegisterPage, expectedUrlRegisterPage);
+        String actualUrlRegisterPage = driver.getCurrentUrl();
+        System.out.println("Url: " + actualUrlRegisterPage);
+        String expectedUrlRegisterPage = "http://live.techpanda.org/index.php/customer/account/create/";
+        Assert.assertEquals(actualUrlRegisterPage, expectedUrlRegisterPage);
 
-    driver.navigate().back();
+        driver.navigate().back();
 
-    String actualUrlLoginPage = driver.getCurrentUrl();
-    System.out.println("Url: " + actualUrlLoginPage);
-    String expectedUrlLoginPage = "http://live.techpanda.org/index.php/customer/account/login/";
-    Assert.assertEquals(actualUrlLoginPage, expectedUrlLoginPage);
+        String actualUrlLoginPage = driver.getCurrentUrl();
+        System.out.println("Url: " + actualUrlLoginPage);
+        String expectedUrlLoginPage = "http://live.techpanda.org/index.php/customer/account/login/";
+        Assert.assertEquals(actualUrlLoginPage, expectedUrlLoginPage);
 
-    driver.navigate().forward();
+        driver.navigate().forward();
 
-    String actualTitleRigisterPage = driver.getTitle();
-    System.out.println("Title: " + actualTitleRigisterPage);
-    String expectedTitleRigisterPage = "Create New Customer Account";
-    Assert.assertEquals(actualTitleRigisterPage, expectedTitleRigisterPage);
-
-    driver.quit();
-    Thread.sleep(3000);
-}
+        String actualTitleRigisterPage = driver.getTitle();
+        System.out.println("Title: " + actualTitleRigisterPage);
+        String expectedTitleRigisterPage = "Create New Customer Account";
+        Assert.assertEquals(actualTitleRigisterPage, expectedTitleRigisterPage);
+    }
 
 }
